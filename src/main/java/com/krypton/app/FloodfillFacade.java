2fc49d8c4c6e93571f045ab439b0ab82b117f54c
CGevorg <CGevorgHarutyunyan@gmail.com>
2016-11-21 21:30:58 +0400

    Adding new UI part

--- src/main/java/com/krypton/app/FloodfillFacade.java
+++ src/main/java/com/krypton/app/FloodfillFacade.java
@@ -2,17 +2,22 @@ package com.krypton.app;
 
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.geometry.VPos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import javax.swing.ButtonGroup;
@@ -20,9 +25,9 @@ import javax.swing.ButtonGroup;
 public class FloodfillFacade extends Application {
 
     private static final int DEFAULT_HBOX_SPACING = 5;
-    private static final int DEFAULT_WINDOW_WIDTH = 400;
+    private static final int DEFAULT_WINDOW_WIDTH = 700;
     private static final int DEFAULT_WINDOW_HEIGHT = 400;
-
+    private static final Insets DEFAULT_SPACING_MAJOR = new Insets(0, 100, 0, 0);
 
     private ButtonGroup modeGroup = new ButtonGroup();
     private ButtonGroup maskGroup = new ButtonGroup();
@@ -46,21 +51,65 @@ public class FloodfillFacade extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-
         StackPane root = new StackPane();
+        VBox vBox = new VBox();
+        vBox.setSpacing(10);
 
-        this.init(root);
+        this.init(vBox);
+        this.initStyleSheet();
 
+        root.getChildren().add(vBox);
         Scene scene = new Scene(root, DEFAULT_WINDOW_WIDTH, DEFAULT_WINDOW_HEIGHT);
         primaryStage.setTitle("Floodfill Window");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
-    public void init(StackPane panel) {
+    public void init(VBox panel) {
         HBox modeBox = new HBox(DEFAULT_HBOX_SPACING);
-        modeBox.setAlignment(Pos.TOP_RIGHT);
+        modeBox.setAlignment(Pos.CENTER_RIGHT);
+        modeBox.setPadding(new Insets(0, 100, 0, 50));
+        Label modeLbl = new Label("Mode:");
+        modeBox.getChildren().addAll(modeLbl,modeColorRbtn,modeGrayscaleRbtn);
 
+        HBox maskBox = new HBox(DEFAULT_HBOX_SPACING);
+        maskBox.setAlignment(Pos.CENTER_RIGHT);
+        maskBox.setPadding(new Insets(0, 100, 0, 50));
+        Label maskLbl = new Label("Mask:");
+        maskBox.getChildren().addAll(maskLbl,maskOnRbtn,maskOffRbtn);
+
+
+        HBox rangeBox = new HBox(DEFAULT_HBOX_SPACING);
+        rangeBox.setAlignment(Pos.CENTER_RIGHT);
+        rangeBox.setPadding(new Insets(0, 100, 0, 50));
+        Label rangeLbl = new Label("Range:");
+        rangeBox.getChildren().addAll(rangeLbl,rangeNullRbtn,rangeFixedRbtn,rangeFloatingRbtn);
+
+        HBox connectivityBox = new HBox(DEFAULT_HBOX_SPACING);
+        connectivityBox.setAlignment(Pos.CENTER_RIGHT);
+        connectivityBox.setPadding(new Insets(0, 100, 0, 50));
+        Label connectivityLbl = new Label("Connectivity:");
+        connectivityBox.getChildren().addAll(connectivityLbl,connectivity4Rbtn,connectivity8Rbtn);
+
+        HBox lowerThBox = new HBox(DEFAULT_HBOX_SPACING);
+        lowerThBox.setAlignment(Pos.CENTER_RIGHT);
+        lowerThBox.setPadding(new Insets(0, 100, 0, 50));
+        Label lowerThLbl = new Label("Lower threshold:");
+        lowerThBox.getChildren().addAll(lowerThLbl,lowerThSlider);
+
+        HBox upperThBox = new HBox(DEFAULT_HBOX_SPACING);
+        upperThBox.setAlignment(Pos.CENTER_RIGHT);
+        upperThBox.setPadding(new Insets(0, 100, 0, 50));
+        Label upperThLbl = new Label("Upper threshold:");
+        upperThBox.getChildren().addAll(upperThLbl,upperThSlider);
+
+
+        panel.getChildren().addAll(modeBox,maskBox,rangeBox,connectivityBox,lowerThBox,upperThBox);
+
+    }
+
+    public void initStyleSheet() {
+        //@TODO
     }
 
     public static void main(String[] args) {
--- src/test/java/com/krypton/app/MorphologyTest.java
+++ src/test/java/com/krypton/app/MorphologyTest.java
@@ -1,19 +1,18 @@
 package com.krypton.app;
 
-import java.awt.Dimension;
-import junit.framework.TestCase;
 import org.junit.Test;
 
-/**
- * Created by employee on 10/22/16.
- */
+import java.awt.Dimension;
+
+
 public class MorphologyTest {
-    static final int SLEEP_DURATION= 7000000;
+    static final int SLEEP_DURATION = 7000000;
+
     @Test
     public void UI() {
         Morphology m = new Morphology();
-        m.setSize(new Dimension(400,300));
-        m.setMinimumSize(new Dimension(400,300));
+        m.setSize(new Dimension(400, 300));
+        m.setMinimumSize(new Dimension(400, 300));
         m.setVisible(true);
         m.openPicture(Morphology.IMAGE_PATH);
         try {
